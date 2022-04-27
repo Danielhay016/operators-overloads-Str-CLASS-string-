4007f7dfca23a09ce752a88adb952eeecbee509e
Daniel Hay <103133855+Danielhay016@users.noreply.github.com>
2022-04-27 16:18:05 +0300

    Add files via upload
    operators-overloads-Str-CLASS-string 
    task 3 - daniel hay C++

---
+++ str.h
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#ifndef _STR_H
+#define _STR_H
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+//Daniel Hay - ID - 209042720
+
+class Str
+{
+	//C'tors & D'tor
+public:
+	Str();
+	Str(const Str& other);
+	Str(const char* str);
+	~Str();
+
+	//Metods - Operators
+public:
+	bool		operator==(const Str& other) const;
+	bool		operator!=(const Str& other) const;
+	bool		operator>(const Str& other) const;
+	bool		operator<(const Str& other) const;
+	const Str&  operator=(const Str& other);
+	const Str&  operator=(const char* str);
+	char&       operator[](int index) const;
+	const Str&  operator++();
+	Str			operator++(int);
+	int		    operator()(char ch) const;
+	Str		    operator+(const Str& other) const;
+	const Str& operator+=(const Str& other);
+
+	//Cast Operators
+public:
+	operator int() const;
+
+	//Friend Operators
+public:
+	friend Str operator+(const char* str, const Str& other);
+	friend Str operator*(int num, const Str& other);
+
+public:
+	const char* getStr() const { return m_str; }
+	//Data Members 
+private:
+	char* m_str;
+};
+
+#endif // _STR_H
+
